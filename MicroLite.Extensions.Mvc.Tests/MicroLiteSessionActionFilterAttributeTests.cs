215453c8d03bac56de83d7cc610d76031944515c
Trevor Pilley <trev@trevorpilley.co.uk>
2012-06-18 23:36:46 +0100

    Started writing the action filter

---
+++ MicroLite.Extensions.Mvc.Tests/MicroLiteSessionActionFilterAttributeTests.cs
@@ -0,0 +1,21 @@
+﻿namespace MicroLite.Extensions.Mvc.Tests
+{
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Unit Tests for the <see cref="MicroLiteSessionActionFilterAttribute"/> class.
+    /// </summary>
+    [TestFixture]
+    public class MicroLiteSessionActionFilterAttributeTests
+    {
+        [Test]
+        public void ConstructorSetsConnectionName()
+        {
+            var connectionName = "Northwind";
+
+            var attribute = new MicroLiteSessionActionFilterAttribute(connectionName);
+
+            Assert.AreEqual(connectionName, attribute.ConnectionName);
+        }
+    }
+}
---
+++ MicroLite.Extensions.Mvc/MicroLiteSessionActionFilterAttribute.cs
@@ -0,0 +1,56 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="MicroLiteSessionActionFilterAttribute.cs" company="MicroLite">
+// Copyright 2012 Trevor Pilley
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// </copyright>
+// -----------------------------------------------------------------------
+namespace MicroLite.Extensions.Mvc
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// An action filter attribute which can be applied to a class or method to supply a <see cref="MicroLiteController"/>
+    /// with a new <see cref="ISession"/> when an action is executed.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = false)]
+    public sealed class MicroLiteSessionActionFilterAttribute : ActionFilterAttribute
+    {
+        private readonly string connectionName;
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="MicroLiteSessionActionFilterAttribute"/> class.
+        /// </summary>
+        public MicroLiteSessionActionFilterAttribute()
+        {
+        }
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="MicroLiteSessionActionFilterAttribute"/> class.
+        /// </summary>
+        /// <param name="connectionName">Name of the connection to manage the session for.</param>
+        public MicroLiteSessionActionFilterAttribute(string connectionName)
+        {
+            this.connectionName = connectionName;
+        }
+
+        /// <summary>
+        /// Gets the name of the connection.
+        /// </summary>
+        public string ConnectionName
+        {
+            get
+            {
+                return this.connectionName;
+            }
+        }
+    }
+}

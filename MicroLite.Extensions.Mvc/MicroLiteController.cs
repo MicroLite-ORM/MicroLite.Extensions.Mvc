3ad1e67724f4d9d34bbb1c52194a5dc7682af441
Trevor Pilley <trev@trevorpilley.co.uk>
2012-06-18 23:29:54 +0100

    Added the MicroLiteController

---
+++ MicroLite.Extensions.Mvc/MicroLiteController.cs
@@ -0,0 +1,44 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="MicroLiteController.cs" company="MicroLite">
+// Copyright 2012 Trevor Pilley
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// </copyright>
+// -----------------------------------------------------------------------
+namespace MicroLite.Extensions.Mvc
+{
+    using System.Web;
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// Provides access to a MicroLite ISession in addition to the base ASP.NET MVC controller.
+    /// </summary>
+    public abstract class MicroLiteController : Controller
+    {
+        /// <summary>
+        /// Gets the System.Web.HttpSessionStateBase object for the current HTTP request.
+        /// </summary>
+        /// <remarks>This property replaces the Controller.Session property so that we can use it for our ISession.</remarks>
+        public HttpSessionStateBase HttpSession
+        {
+            get
+            {
+                return base.Session;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the <see cref="ISession"/> for the current HTTP request.
+        /// </summary>
+        public new ISession Session
+        {
+            get;
+            set;
+        }
+    }
+}

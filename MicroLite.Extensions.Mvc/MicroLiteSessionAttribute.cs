265dc8c56f35a6fa40aad22d84aa77b874463c40
Trevor Pilley <trev@trevorpilley.co.uk>
2013-05-22 17:50:16 +0100

    Closes #14

--- MicroLite.Extensions.Mvc/MicroLiteSessionAttribute.cs
+++ MicroLite.Extensions.Mvc/MicroLiteSessionAttribute.cs
@@ -145,8 +145,6 @@ namespace MicroLite.Extensions.Mvc
                 this.sessionManager.OnActionExecuting(readOnlyController.Session, this.AutoManageTransaction, this.IsolationLevel);
                 return;
             }
-
-            throw new NotSupportedException(ExceptionMessages.ControllerNotMicroLiteController);
         }
 
         private ISessionFactory FindSessionFactoryForSpecifiedConnection()

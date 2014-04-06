fa7cca494b237d1ff16deab34451775a4f529ee0
Trevor Pilley <trev@trevorpilley.co.uk>
2014-04-06 19:07:18 +0100

    Closes #15

---
+++ MicroLite.Extensions.Mvc.Tests/ValidateModelNotNullAttributeTests.cs
@@ -0,0 +1,76 @@
+﻿namespace MicroLite.Extensions.Mvc.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Web.Mvc;
+    using MicroLite.Extensions.Mvc;
+    using Xunit;
+
+    public class ValidateModelNotNullAttributeTests
+    {
+        public class WhenCallingOnActionExecuting_AndTheActionArgumentsDoesNotContainNull
+        {
+            private readonly ValidateModelNotNullAttribute attribute = new ValidateModelNotNullAttribute();
+
+            [Fact]
+            public void NoExceptionShouldBeThrown()
+            {
+                var actionContext = new ActionExecutingContext
+                {
+                    ActionParameters = new Dictionary<string, object>
+                    {
+                        { "model", new object() }
+                    }
+                };
+
+                Assert.DoesNotThrow(() => attribute.OnActionExecuting(actionContext));
+            }
+        }
+
+        public class WhenCallingOnActionExecuting_TheActionArgumentsContainNull_AndSkipValidationIsFalse
+        {
+            private readonly ValidateModelNotNullAttribute attribute = new ValidateModelNotNullAttribute
+            {
+                SkipValidation = false
+            };
+
+            [Fact]
+            public void AnArgumentNullExceptionShouldBeThrown()
+            {
+                var actionContext = new ActionExecutingContext
+                {
+                    ActionParameters = new Dictionary<string, object>
+                    {
+                        { "model", null }
+                    }
+                };
+
+                var exception = Assert.Throws<ArgumentNullException>(() => attribute.OnActionExecuting(actionContext));
+
+                Assert.Equal("model", exception.ParamName);
+            }
+        }
+
+        public class WhenCallingOnActionExecuting_TheActionArgumentsContainNull_AndSkipValidationIsTrue
+        {
+            private readonly ValidateModelNotNullAttribute attribute = new ValidateModelNotNullAttribute
+            {
+                SkipValidation = true
+            };
+
+            [Fact]
+            public void NoExceptionShouldBeThrown()
+            {
+                var actionContext = new ActionExecutingContext
+                {
+                    ActionParameters = new Dictionary<string, object>
+                    {
+                        { "model", null }
+                    }
+                };
+
+                Assert.DoesNotThrow(() => attribute.OnActionExecuting(actionContext));
+            }
+        }
+    }
+}
---
+++ MicroLite.Extensions.Mvc/ValidateModelNotNullAttribute.cs
@@ -0,0 +1,58 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ValidateModelNotNullAttribute.cs" company="MicroLite">
+// Copyright 2012 - 2014 Project Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// </copyright>
+// -----------------------------------------------------------------------
+namespace MicroLite.Extensions.Mvc
+{
+    using System;
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// An <see cref="ActionFilterAttribute"/> which verifies the parameters passed to the controller action are not null.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method, Inherited = true, AllowMultiple = false)]
+    public sealed class ValidateModelNotNullAttribute : ActionFilterAttribute
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether to skip validation (false by default).
+        /// </summary>
+        /// <remarks>
+        /// Allows overriding the default behaviour on an individual action/controller if an instance
+        /// is already registered in the global filters.
+        /// </remarks>
+        public bool SkipValidation
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Called by the ASP.NET MVC framework before the action method executes.
+        /// </summary>
+        /// <param name="filterContext">The filter context.</param>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:Validate arguments of public methods", MessageId = "0", Justification = "This method is only called the MVC framework & the ActionExecutingContext should never be null.")]
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (this.SkipValidation)
+            {
+                return;
+            }
+
+            foreach (var kvp in filterContext.ActionParameters)
+            {
+                if (kvp.Value == null)
+                {
+                    throw new ArgumentNullException(kvp.Key);
+                }
+            }
+        }
+    }
+}

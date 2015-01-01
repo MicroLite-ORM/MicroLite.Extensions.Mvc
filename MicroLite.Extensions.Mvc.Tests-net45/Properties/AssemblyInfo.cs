e68e000d9f6fc480647ddb5163dd0fde05553ade
Trevor Pilley <trev@trevorpilley.co.uk>
2015-01-01 18:33:45 +0000

    Built MicroLite.Extensions.Mvc 6.1.0

--- MicroLite.Extensions.Mvc.Tests-net45/Properties/AssemblyInfo.cs
+++ MicroLite.Extensions.Mvc.Tests-net45/Properties/AssemblyInfo.cs
@@ -27,5 +27,5 @@ using System.Runtime.InteropServices;
 //      Minor Version
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("6.0.0.0")]
-[assembly: AssemblyFileVersion("6.0.0.0")]
+[assembly: AssemblyVersion("6.1.0.0")]
+[assembly: AssemblyFileVersion("6.1.0.0")]
